042f375f5a16afe10c05740f35322f1f4724cf2e
Volodymyr Donets <volodons@gmail.com>
2023-07-14 13:17:56 +0300

    Implement Counter Functional Component

---
+++ task3_1/src/components/CounterFunctional.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function CounterFunctional() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div>
+      <h1>Functional Counter</h1>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+    </div>
+  );
+}
+
+export { CounterFunctional };
--- task3_1/src/main.jsx
+++ task3_1/src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { CounterClass } from "./components/CounterClass.jsx";
+import { CounterFunctional } from "./components/CounterFunctional.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CounterClass />
+    <CounterFunctional />
   </React.StrictMode>
 );
