1673c2e18f4a770ed5846ff8df4ebaf975ec7339
Volodymyr Donets <volodons@gmail.com>
2023-07-14 12:51:04 +0300

    Implement Counter Class Component

---
+++ task3_1/src/components/CounterClass.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+class CounterClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  decrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Class Counter</h1>
+        <button onClick={this.decrement}>-</button>
+        <span>{this.state.count}</span>
+        <button onClick={this.increment}>+</button>
+      </div>
+    );
+  }
+}
+
+export { CounterClass };
