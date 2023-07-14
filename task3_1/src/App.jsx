668a0aaa4f7d0030982a0f128d7467776c1eb50d
Volodymyr Donets <volodons@gmail.com>
2023-07-14 13:49:34 +0300

    Put Class and Functional Counter Components into one App Component

--- task3_1/src/App.jsx
+++ task3_1/src/App.jsx
@@ -1,1 +1,14 @@
+import CounterClass from "./components/CounterClass.jsx";
+import CounterFunctional from "./components/CounterFunctional.jsx";
+import "./App.css";
 
+function App() {
+  return (
+    <>
+      <CounterClass />
+      <CounterFunctional />
+    </>
+  );
+}
+
+export default App;
